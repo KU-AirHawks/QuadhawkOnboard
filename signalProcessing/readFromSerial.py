f4b2c04861ae2e505e36c19fb7b8a68323fdf659
EliLoney <eliloney@ku.edu>
2024-03-01 12:36:45 -0600

    Edited readFromSerial

--- signalProcessing/readFromSerial.py
+++ signalProcessing/readFromSerial.py
@@ -1,17 +1,23 @@
-import serial
+#import rospy
+#import serial
+#from std_msgs.msg import String
 
-def serialRead(port, baudrate=9600):
-    ser = serial.Serial(port, baudrate)
+# Old code may need to use (parts) during actual implementation
+
+#def serialRead(port, baudrate=9600):
+ #   ser = serial.Serial(port, baudrate)
     
-    try:
-        while True:
-            if ser.in_waiting >0:
-                data = ser.readline().decode().strip()
-                print("Received:", data)
-    except KeyboardInterrupt:
-        print("Closing...")
-        ser.close()
+  #  try:
+   #     while True:
+    #        if ser.in_waiting >0:
+     #           data = ser.readline().decode().strip()
+      #          print("Received:", data)
+    #except KeyboardInterrupt:
+    #    print("Closing...")
+    #    ser.close()
         
-if __name__ == "__main__":
-    serial_port = "dev/ttyUSB4"  # Replace with serial port
-    serialRead(serial_port)
+#if __name__ == "__main__":
+#    serial_port = "dev/ttyUSB4"  # Replace with serial port
+#    serialRead(serial_port)
+
+#!/usr/bin/env python
